adfb870b4669f9c7daf013bd965aee7de8f1c05c
Justin Formentin <40323577+justinformentin@users.noreply.github.com>
2018-07-26 00:29:26 -0400

    Made Chart Responsive

--- src/views/CryptoChart/components/PriceChart.jsx
+++ src/views/CryptoChart/components/PriceChart.jsx
@@ -29,42 +29,44 @@ const PriceChart = props => (
       {props.invalidDateRange ? (
         <ErrorMessage />
       ) : (
-        <AreaChart
-          width={props.width}
-          height={props.height}
-          data={props.priceData}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#20a8d8" stopOpacity={0.9}/>
-              <stop offset="95%" stopColor="#20a8d8" stopOpacity={0.6}/>
-            </linearGradient>
-          </defs>  
-          <XAxis
-            dataKey="date"
-            padding={{ left: 10 }}
-            minTickGap={30}
-            tickFormatter={props.formatDateScales}
-          />
-          <YAxis
-            type="number"
-            domain={['auto', 'auto']}
-            label={{ value: 'Price (USD)', angle: -90, position: 'insideLeft' }}
-            
-          />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip labelFormatter={props.formatTooltipLabel} />
-          <Area
-            type="monotone"
-            dataKey="Price (USD)"
-            stroke="#20a8d8"
-            fillOpacity={1} 
-            fill="url(#colorUv)"
-            isAnimationActive={false}
-            dot={false}
-          />
-        </AreaChart>
+        <ResponsiveContainer width="98%" height={props.height}>
+          <AreaChart
+            width={props.width}
+            height={props.height}
+            data={props.priceData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#20a8d8" stopOpacity={0.9}/>
+                <stop offset="95%" stopColor="#20a8d8" stopOpacity={0.6}/>
+              </linearGradient>
+            </defs>  
+            <XAxis
+              dataKey="date"
+              padding={{ left: 10 }}
+              minTickGap={30}
+              tickFormatter={props.formatDateScales}
+            />
+            <YAxis
+              type="number"
+              domain={['auto', 'auto']}
+              label={{ value: 'Price (USD)', angle: -90, position: 'insideLeft' }}
+
+            />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip labelFormatter={props.formatTooltipLabel} />
+            <Area
+              type="monotone"
+              dataKey="Price (USD)"
+              stroke="#20a8d8"
+              fillOpacity={1} 
+              fill="url(#colorUv)"
+              isAnimationActive={false}
+              dot={false}
+            />
+          </AreaChart>
+        <ResponsiveContainer>  
       )}
     </div>
   </div>
